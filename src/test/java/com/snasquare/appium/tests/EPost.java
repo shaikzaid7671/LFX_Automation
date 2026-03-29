b9e2836cb83b4957e2a6a945719bd69d3e23238c
shaikzaid7671 <shaikzaid9197@gmail.com>
2026-03-29 17:31:43 +0530

    Update EPost.java

--- src/test/java/com/snasquare/appium/tests/EPost.java
+++ src/test/java/com/snasquare/appium/tests/EPost.java
@@ -37,10 +37,11 @@ public class EPost {
         cap.setCapability("noReset", true);
         cap.setCapability("newCommandTimeout", 300);
 
+         // ✅ FIXED (removed local variable)
         driver = new AndroidDriver(
-    new URL("http://host.docker.internal:4723/wd/hub"),
-    options
-    );
+                new URL("http://127.0.0.1:4723"),
+                cap
+        );
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
