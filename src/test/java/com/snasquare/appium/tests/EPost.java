91f66dc1d467e4d2d877b9ba41ca357692a703c9
shaikzaid7671 <shaikzaid9197@gmail.com>
2026-03-29 12:07:43 +0530

    Update EPost.java

--- src/test/java/com/snasquare/appium/tests/EPost.java
+++ src/test/java/com/snasquare/appium/tests/EPost.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.net.URL;
 import java.time.Duration;
